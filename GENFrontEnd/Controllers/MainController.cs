3d37cd2453732a0a9760be3f7f4773f5f62e7d4d
hendrasetiadinata <hendrasetiadinata@DINATAPC>
2017-03-31 21:44:34 +0700

    Add project files.

---
+++ GENFrontEnd/Controllers/MainController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLogic;
+using GENFrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace GENFrontEnd.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        BusinessLogic.Model.Message message = new BusinessLogic.Model.Message();
+        BusinessLogic.Model.Email email = new BusinessLogic.Model.Email();
+        Models.EntityManager.EmailManager emailManager = new Models.EntityManager.EmailManager();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Default()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            ViewData["MAPS_API_KEY"] = Util.getProperty("MAPS_API_KEY");
+            return View();
+        }
+        public ActionResult Program()
+        {
+            return View();
+        }
+        public ActionResult PartialSectionContact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public String SendQuestion(string SenderFullName, string SenderEmail, string SenderPhone, string SenderQuestion)
+        {
+            string Result = "";
+
+            if (SenderFullName != null && SenderEmail != null && SenderPhone != null && SenderQuestion != null)
+            {
+                string SenderIP = System.Web.HttpContext.Current.Request.UserHostAddress;
+                email = email.setSaveEmail(SenderFullName, SenderPhone, SenderEmail, SenderQuestion, SenderIP);
+                message = emailManager.insert_Email(email);
+                Result = JsonConvert.SerializeObject(message);
+            }
+            return Result;
+        }
+    }
+}
---
+++ GENFrontEnd/Models/EntityManager/EmailManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessLogic;
+using GENFrontEnd.Models.DB;
+using System.Data.Linq.Mapping;
+using Microsoft.Ajax.Utilities;
+using BusinessLogic.Model;
+
+namespace GENFrontEnd.Models.EntityManager
+{
+    public class EmailManager
+    {
+        Message msg = new Message();
+
+        public Message insert_Email (BusinessLogic.Model.Email email)
+        {
+            try
+            {
+                TREMAIL trEmail = new TREMAIL();
+                trEmail.EmailID = getEmailID();
+                trEmail.SenderFullName = email.SenderFullName;
+                trEmail.SenderEmail = email.SenderEmail;
+                trEmail.SenderPhone = email.SenderPhone;
+                trEmail.SenderQuestion = email.SenderQuestion;
+                trEmail.SenderIP = email.SenderIP;
+                trEmail.CreatedDate = DateTime.Now;
+                GENEntities db = new GENEntities();
+                db.TREMAILs.Add(trEmail);
+                if (db.SaveChanges() > 0)
+                {
+                    msg.setMessage("MSG0001", "Data saved successfully", null);
+                }
+                else
+                {
+                    msg.setMessage("MSG0002", "Data could not be saved", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                msg.setMessage(null, ex.Message, "ERR0001");
+            }
+            return msg;
+        }
+        public Message update_Email(BusinessLogic.Model.Email email)
+        {
+            try
+            {
+                TREMAIL trEmail = new TREMAIL();
+                trEmail.EmailID = 0;
+                trEmail.IsReply = 1;
+                trEmail.ReplyDate = DateTime.Now;
+                trEmail.ReplyMessage = email.ReplyMessage;
+                trEmail.CreatedBy = email.CreatedBy;
+                trEmail.EmailCategory = email.EmailCategory;
+                GENEntities db = new GENEntities();
+                db.TREMAILs.Add(trEmail);
+                if (db.SaveChanges() > 0)
+                {
+                    msg.setMessage("MSG0001", "Data updated successfully", null);
+                }
+                else
+                {
+                    msg.setMessage("MSG0002", "Data could not be updated", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                msg.setMessage(null, ex.Message, "ERR0001");
+            }
+            return msg;
+        }
+
+        public Int32 getEmailID()
+        {
+            var emailID = "";
+            using (var db = new GENEntities())
+            {
+                try
+                {
+                    emailID = db.Database.SqlQuery<string>(String.Format(@"select dbo.GetEmailId()")).SingleOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    msg.setMessage(null, ex.Message, "ERR0001");
+                }
+            }
+            return Convert.ToInt32(emailID);
+        }
+    }
+}
